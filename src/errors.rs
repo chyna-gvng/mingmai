3f36d8f95546f34862329b3d9aa9892802f3333f
chyna-gvng <angoyewally@outlook.com>
2025-09-15 00:18:49 +0300

    add: src/errors.rs

---
+++ src/errors.rs
@@ -0,0 +1,17 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum MingmaiError {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Serde error: {0}")]
+    Serde(#[from] serde_json::Error),
+    #[error("Other: {0}")]
+    Other(String),
+}
+
+impl From<anyhow::Error> for MingmaiError {
+    fn from(e: anyhow::Error) -> Self {
+        MingmaiError::Other(e.to_string())
+    }
+}
