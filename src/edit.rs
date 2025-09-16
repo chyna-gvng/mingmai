95b65efe4b87500edef5e361ea7df14428998323
chyna-gvng <angoyewally@outlook.com>
2025-09-16 16:37:08 +0300

    mod: src/edit.rs

--- src/edit.rs
+++ src/edit.rs
@@ -1,5 +1,5 @@
-use serde::{Deserialize, Serialize};
 use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, JsonSchema)]
 pub struct Position {
